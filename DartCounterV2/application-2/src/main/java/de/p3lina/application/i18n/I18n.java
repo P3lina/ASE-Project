ac246fab4bd10d02f8984f2cf40a95aa304f7141
P3lina <purschwitzplay@gmail.com>
2023-03-28 21:52:46 +0200

    I18n works

--- DartCounterV2/application-2/src/main/java/de/p3lina/application/i18n/I18n.java
+++ DartCounterV2/application-2/src/main/java/de/p3lina/application/i18n/I18n.java
@@ -2,12 +2,15 @@ package de.p3lina.application.i18n;
 
 import de.p3lina.domain.i18n.Messages;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class I18n {
 
-    public static String getMessage(Messages message) {
-        ResourceBundle rb = ResourceBundle.getBundle("messages.resource_bundle");
+    private static final String BUNDLE_NAME = "de.p3lina.messages.resource_bundle";
+
+    public String getMessage(Messages message, Locale locale) {
+        ResourceBundle rb = ResourceBundle.getBundle(BUNDLE_NAME, locale);
         return rb.getString(message.toString());
     }
 }
